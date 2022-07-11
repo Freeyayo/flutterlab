d628d8e2db8b9d039cdb6ccaae00ed490a737182
Kevin Cai <kevin_cai@epam.com>
2022-07-11 17:42:38 +0800

    stateless widget & hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget{
+  const Home ({Key? key}) : super(key: key);
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text("Hello World"),
         centerTitle: true,
@@ -25,6 +33,6 @@ void main() {
         backgroundColor: Colors.red[600],
         child: const Text('click'),
       ),
-    ),
-  ));
+    );
+  }
 }
