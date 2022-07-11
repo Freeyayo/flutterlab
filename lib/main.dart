f02e6a3d47065325af103b768c3d689a67900957
Kevin Cai <kevin_cai@epam.com>
2022-07-11 16:54:26 +0800

    styling & import fonts

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,23 @@ void main() {
       appBar: AppBar(
         title: const Text("Hello World"),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: const Center(
-        child: Text('This is body'),
+        child: Text(
+          'This is body',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'IndieFlower',
+            letterSpacing: 2.0,
+            color: Colors.grey,
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
         child: const Text('click'),
       ),
     ),
